ebc3cdea1c476af7a0e35333c650344bae8fc3aa
WearTime <rizqiwiransyah0410@gmail.com>
2025-10-08 02:36:55 +0700

    Fix: Some Errors

--- Untitled-1.rb
+++
@@ -1,9 +0,0 @@
-What is your project named? rumah-sekolah-modern
-Would you like to use TypeScript? Yes
-Would you like to use ESLint? Yes
-Would you like to use Tailwind CSS? No
-Would you like your code inside a `src/` directory? Yes
-Would you like to use App Router? (recommended) Yes
-Would you like to use Turbopack? (recommended) Yes
-Would you like to customize the import alias (`@/*` by default)? Yes
-What import alias would you like configured? WT/*
